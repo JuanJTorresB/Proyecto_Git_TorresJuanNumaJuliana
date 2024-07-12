6a181e83bd293cdfe1183bef849d9ce50f7d79ab
Juliana0121 <juliananuma3@gmail.com>
2024-07-12 15:34:38 -0500

    feat(Modulo Menus): :sparkles: Impresiones de menus
    Se crearon todos los menus correspondientes al proyecto

--- Modulos/Menus.py
+++ Modulos/Menus.py
@@ -1,2 +1,46 @@
 def Saludo_User():
     print("Bienvenido User")
+
+def menu_principal():
+    print("-----------Menú principal-----------")   
+    print("Digita [1] para --> Agregar cuidad")
+    print("Digita [2] para --> Buscar ciudad")
+    print("Digita [3] para --> Editar ciudad existente")
+    print("Digita [0] para --> Salir")
+    print("-----------------------------------------------")
+
+def menu_agregar_ciudad():
+    print("-------------Menú de Agregar ciudad-------------")  
+    print("Digita [1] para --> Ingrese el nombre")
+    print("Digita [2] para --> Ingrese el codigo postal")
+    print("Digita [3] para --> Ingrese la poblacion estimada")
+    print("Digita [4] para --> Ingrese el pais")
+    print("Digita [0] para --> Regresar al menu principal")
+    print("-----------------------------------------------")
+    
+def menu_buscar_ciudad():
+    print("-------------Menú de Buscar ciudad-------------")  
+    print("Digita [1] para --> Buscar por nombre")
+    print("Digita [2] para --> Buscar por pais")
+    print("Digita [3] para --> Buscar por codigo postal")
+    print("Digita [0] para --> Regresar al menu principal")
+    print("-----------------------------------------------")
+        
+def menu_editar_ciudad():
+    print("-------------Menú de Editar Ciudad-------------")  
+    print("Digita [1] para --> Cambiar nombre")
+    print("Digita [2] para --> Cambiar codigo postal")
+    print("Digita [3] para --> Cambiar poblacion estimada")
+    print("Digita [4] para --> Cambiar pais")
+    print("Digita [0] para --> Regresar al menu principal")
+    print("-----------------------------------------------")
+    
+    
+def pedir_opc():
+    opc = 0
+    try:
+        opc = int(input("-->Digita la opción a elegir: "))
+        return opc
+    except Exception:
+        print("Valor inválido")
+        return 0
